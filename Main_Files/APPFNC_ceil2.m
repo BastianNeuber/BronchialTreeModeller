0d85c284e83577134d7e6e77cc6ce8e3d350959b
BastianNeuber <132922793+BastianNeuber@users.noreply.github.com>
2023-05-22 10:19:44 +0200

    Delete Main_Files directory

--- Main_Files/APPFNC_ceil2.m
+++
@@ -1,7 +0,0 @@
-function [x] = APPFNC_ceil2(x)
-x = ceil(x);
-if x < 2
-    x = 2;
-end
-end
-
--- Main_Files/ILLFNC_Winkelbogen.m
+++
@@ -1,6 +0,0 @@
-function [K,x,y] = FNC_Winkelbogen(M,r,Startwinkel,Winkel,Pos,Xlim,Ylim)
-
-K = FNC_Kreisbogen2(M,r,Startwinkel,Winkel);
-[x,y] = FNC_AnnotCoord(Pos,Xlim,Ylim,K(99,:),K(100,:));
-
-end
